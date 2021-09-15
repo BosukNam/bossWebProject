87c0ec04bfa019b5ba682334545d37b37f29dc29
BossNam <popman1105@gmail.com>
2021-09-15 17:15:08 +0900

    user 중복 로그인 방지 로직 추가 (static map으로 로그인 유저 관리)

--- src/main/java/com/boss/common/listener/SessionAttributeListener.java
+++ src/main/java/com/boss/common/listener/SessionAttributeListener.java
@@ -1,30 +1,42 @@
 package com.boss.common.listener;
 
-import java.util.Enumeration;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
+
+@Slf4j
 @Component
 public class SessionAttributeListener implements HttpSessionAttributeListener {
-    public static ConcurrentHashMap<HttpSession, String> sessionMap = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<String, HttpSession> sessionMap = new ConcurrentHashMap<>();
     public void init(ServletConfig config) {
     }
     @Override
     public void attributeAdded(HttpSessionBindingEvent event) {
         HttpSession session = event.getSession();
         String attributeName = event.getName();
-        if(attributeName.equals("SPRING_SECURITY_CONTEXT")){
+
+        if("SPRING_SECURITY_CONTEXT".equals(attributeName)){
             if(event.getValue() instanceof SecurityContextImpl) {
                 SecurityContextImpl securityContextImpl = (SecurityContextImpl) event.getValue();
                 Authentication authentication =securityContextImpl.getAuthentication();
                 String userId = authentication.getName();
-                if(sessionMap.get(session) == null) {
-                    sessionMap.put(session, userId);
+                if(sessionMap.get(userId)==null) {
+                    sessionMap.put(userId, session);
+                } else if(sessionMap.get(userId)!=null){
+                    // TODO : 기존 로그인 유저를 invalid 할 것인지, 신규 로그인 유저를 invalid 할 것인지 등 고민
+                    session.invalidate();
+                    try {
+                        sessionMap.get(userId).invalidate();
+                    } catch (NullPointerException ex) {
+                        log.error("session invalidated already!");
+                    }
                 }
             }
         }
@@ -32,27 +44,18 @@ public class SessionAttributeListener implements HttpSessionAttributeListener {
     @Override
     public void attributeRemoved(HttpSessionBindingEvent event) {
         String attributeName = event.getName();
-        HttpSession session = event.getSession();
         if (attributeName.equals("SPRING_SECURITY_CONTEXT")) {
             if(event.getValue() instanceof SecurityContextImpl) {
-                if(sessionMap.get(session) != null) {
-                    sessionMap.remove(session);
+                SecurityContextImpl securityContextImpl = (SecurityContextImpl) event.getValue();
+                Authentication authentication =securityContextImpl.getAuthentication();
+                String userId = authentication.getName();
+                if(sessionMap.get(userId) != null) {
+                    sessionMap.remove(userId);
                 }
             }
         }
     }
     @Override
     public void attributeReplaced(HttpSessionBindingEvent event) {
-    }
-
-    private void duplicateCheck(String userId) {
-        ConcurrentHashMap<HttpSession, String> sessionMap = SessionAttributeListener.sessionMap;
-        Enumeration<HttpSession> keys = sessionMap.keys();
-        while (keys.hasMoreElements()) {
-            HttpSession session = (HttpSession) keys.nextElement();
-            if(sessionMap.get(session).equals(userId)){
-                session.invalidate();
-            }
-        }
     }
 }
