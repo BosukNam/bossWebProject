f23d253fe42b17301c4e2c96691ac2e43b702df3
BossNam <popman1105@gmail.com>
2021-10-02 16:58:29 +0900

    redis 설정 추가 (greeting 으로 들어오는 키-값 저장 + welcome 페이지에 key값 출력)

--- src/main/java/com/boss/common/hateoas/greeting/ctrl/GreetingRestController.java
+++ src/main/java/com/boss/common/hateoas/greeting/ctrl/GreetingRestController.java
@@ -3,6 +3,8 @@ package com.boss.common.hateoas.greeting.ctrl;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import com.boss.common.hateoas.greeting.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,14 @@ public class GreetingRestController {
 
     private static final String TEMPLATE = "Hello, %s!";
 
+    @Autowired
+    private RedisTemplate redisStringTemplate;
+
     @RequestMapping("/greeting")
     public HttpEntity<Greeting> greeting(
             @RequestParam(value = "name", defaultValue = "World") String name) {
 
+        redisStringTemplate.opsForValue().set(name, "hello");
         Greeting greeting = new Greeting(String.format(TEMPLATE, name));
         greeting.add(linkTo(methodOn(GreetingRestController.class).greeting(name)).withSelfRel());
 
--- src/main/java/com/boss/common/login/ctrl/LoginController.java
+++ src/main/java/com/boss/common/login/ctrl/LoginController.java
@@ -1,12 +1,22 @@
 package com.boss.common.login.ctrl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class LoginController {
+
+    @Autowired
+    private RedisTemplate redisStringTemplate;
+
     @GetMapping("/")
-    public String indexPage() {
+    public String indexPage(Model model) {
+        model.addAttribute("redisKeys", redisStringTemplate.keys("*"));
         return "welcome";
     }
 
